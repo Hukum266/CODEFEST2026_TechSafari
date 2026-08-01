89839ab5315d5a560b175620c45eb8e882f475d4
hukum266 <hukumroka266@gmail.com>
2026-08-01 06:02:18 +0545

    UI about to set

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+ tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          paper: '#F3F5EC',
+          paperdeep: '#E7EBDA',
+          ink: '#1C2618',
+          inksoft: '#4A5741',
+          canopy: '#2F5233',
+          canopylight: '#4F7A52',
+          soil: '#8B5E34',
+          irrigation: '#2F6690',
+          amber: '#B8710F',
+          rust: '#A8452F',
+          line: '#D4DAC4',
+        },
+        fontFamily: {
+          display: ['"Space Grotesk"', 'sans-serif'],
+          body: ['Inter', 'sans-serif'],
+          mono: ['"IBM Plex Mono"', 'monospace'],
+        },
+      },
+    },
+  };
